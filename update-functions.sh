c8a4f7a9241fc8025d1bfa1aac521de257fcccd6
vahidkhorasani <vh.khorasani@gmail.com>
2021-03-19 01:50:20 +0330

    Fix dev/stage/prod env files parameters location

--- update-functions.sh
+++ update-functions.sh
@@ -2,30 +2,45 @@ function dev_update {
 	for i in $(find $WD/$line/ci/k8s/ -type f -iname "*$line*env-dev")
 		do
 		drpl_check=$(grep -c 'RPL' $i)
-		if [[ $rpl_check == 1 ]]; then
+		if [[ $rpl_check == 0 ]]; then
+			declare -i DREV_LINE="2"
+			declare -i DMEM_LINE="3"
+			declare -i DCPU_LINE="4"
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '1 s/$/\n/' $i
+				printf "%*s%s" $x '' "$drev_lim" | sed -i "${DREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$dmem_lim" | sed -i "${DMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$dcpu_lim" | sed -i "${DCPU_LINE}"'e cat /dev/stdin' $i
+			fi
+		elif [[ $rpl_check == 1 ]]; then
 			declare -i DRPL_LINE=$(cat $i | nl | grep RPL | cut -d$'\t' -f1 | sed  's/^ *//g')
 			declare -i DREV_LINE="$DRPL_LINE+1"
 			declare -i DMEM_LINE="$DRPL_LINE+2"
 			declare -i DCPU_LINE="$DRPL_LINE+3"
-		else 
-			declare -i DREV_LINE="1"
-			declare -i DMEM_LINE="2"
-			declare -i DCPU_LINE="3"
-		fi
-		w=$(grep -c REV_HIST_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RPL/ a \ \' $i
-			printf "%*s%s" $x '' "$drev_lim" | sed -i "${DREV_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_MEM_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/REV_HIST_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$dmem_lim" | sed -i "${DMEM_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_CPU_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RSRC_MEM_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$dcpu_lim" | sed -i "${DCPU_LINE}"'e cat /dev/stdin' $i
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RPL/ a \ \' $i
+				printf "%*s%s" $x '' "$drev_lim" | sed -i "${DREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$dmem_lim" | sed -i "${DMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$dcpu_lim" | sed -i "${DCPU_LINE}"'e cat /dev/stdin' $i
+			fi
 		fi
         done
 }
@@ -33,30 +48,45 @@ function stage_update {
 		for i in $(find $WD/$line/ci/k8s/ -iname "*$line*env-stage")
         do
 		srpl_check=$(grep -c 'RPL' $i)
-		if [[ $srpl_check == 1 ]]; then
+		if [[ $srpl_check == 0 ]]; then
+			declare -i SREV_LINE="2"
+			declare -i SMEM_LINE="3"
+			declare -i SCPU_LINE="4"
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '1 s/$/\n/' $i
+				printf "%*s%s" $x '' "$srev_lim" | sed -i "${SREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$smem_lim" | sed -i "${SMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$scpu_lim" | sed -i "${SCPU_LINE}"'e cat /dev/stdin' $i
+			fi
+		elif [[ $srpl_check == 1 ]]
 			declare -i SRPL_LINE=$(cat $i | nl | grep RPL | cut -d$'\t' -f1 | sed  's/^ *//g')
 			declare -i SREV_LINE="$SRPL_LINE+1"
 			declare -i SMEM_LINE="$SRPL_LINE+2"
 			declare -i SCPU_LINE="$SRPL_LINE+3"
-		else
-			declare -i SREV_LINE="1"
-			declare -i SMEM_LINE="2"
-			declare -i SCPU_LINE="3"
-		fi
-		w=$(grep -c REV_HIST_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RPL/ a \ \' $i
-			printf "%*s%s" $x '' "$srev_lim" | sed -i "${SREV_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_MEM_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/REV_HIST_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$smem_lim" | sed -i "${SMEM_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_CPU_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RSRC_MEM_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$scpu_lim" | sed -i "${SCPU_LINE}"'e cat /dev/stdin' $i
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RPL/ a \ \' $i
+				printf "%*s%s" $x '' "$srev_lim" | sed -i "${SREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$smem_lim" | sed -i "${SMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$scpu_lim" | sed -i "${SCPU_LINE}"'e cat /dev/stdin' $i
+			fi
 		fi
         done
 }
@@ -64,30 +94,45 @@ function prod_update {
 		for i in $(find $WD/$line/ci/k8s/ -iname "*$line*env-prod")
         do
 		prpl_check=$(grep -c 'RPL' $i)
-		if [[ $prpl_check == 1 ]]; then
+		if [[ $prpl_check == 0 ]]; then
+			declare -i PREV_LINE="1"
+			declare -i PMEM_LINE="2"
+			declare -i PCPU_LINE="3"
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '1 s/$/\n/' $i
+				printf "%*s%s" $x '' "$prev_lim" | sed -i "${PREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$pmem_lim" | sed -i "${PMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$pcpu_lim" | sed -i "${PCPU_LINE}"'e cat /dev/stdin' $i
+			fi
+		elif [[ $prpl_check == 1 ]]; then
 			declare -i PRPL_LINE=$(cat $i | nl | grep RPL | cut -d$'\t' -f1 | sed  's/^ *//g')
 			declare -i PREV_LINE="$PRPL_LINE+1"
 			declare -i PMEM_LINE="$PRPL_LINE+2"
 			declare -i PCPU_LINE="$PRPL_LINE+3"
-		else
-			declare -i PREV_LINE="1"
-			declare -i PMEM_LINE="2"
-			declare -i PCPU_LINE="3"
-		fi
-		w=$(grep -c REV_HIST_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RPL/ a \ \' $i
-			printf "%*s%s" $x '' "$prev_lim" | sed -i "${PREV_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_MEM_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/REV_HIST_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$pmem_lim" | sed -i "${PMEM_LINE}"'e cat /dev/stdin' $i
-		fi
-		w=$(grep -c RSRC_CPU_LIM $i)
-		if [[ $w == 0 ]]; then
-			sed -i '/RSRC_MEM_LIM/ a \ \' $i
-			printf "%*s%s" $x '' "$pcpu_lim" | sed -i "${PCPU_LINE}"'e cat /dev/stdin' $i
+			w=$(grep -c REV_HIST_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RPL/ a \ \' $i
+				printf "%*s%s" $x '' "$prev_lim" | sed -i "${PREV_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_MEM_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/REV_HIST_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$pmem_lim" | sed -i "${PMEM_LINE}"'e cat /dev/stdin' $i
+			fi
+			w=$(grep -c RSRC_CPU_LIM $i)
+			if [[ $w == 0 ]]; then
+				sed -i '/RSRC_MEM_LIM/ a \ \' $i
+				printf "%*s%s" $x '' "$pcpu_lim" | sed -i "${PCPU_LINE}"'e cat /dev/stdin' $i
+			fi
 		fi
         done
 }
